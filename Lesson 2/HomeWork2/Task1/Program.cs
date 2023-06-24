705955006f3cc139421afbb889a41a6f63fc9f4c
Vlada <vlada@tar.ru>
2023-06-25 02:11:09 +0300

    Домашнаяя задача 1

--- Lesson 2/HomeWork2/Task1/Program.cs
+++ Lesson 2/HomeWork2/Task1/Program.cs
@@ -4,3 +4,32 @@
 // 918 -> 1
 
 Console.Clear();
+
+/* 
+Функция вводит целое значение из консоли
+*/
+
+int ReadInt(string message)
+{
+    System.Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());   // Чтение строки и преобразование в число
+    return number;                                      // Возврат целого числа
+}
+
+bool Validate3sign(int number)
+{
+    if (number < 100 || number >= 1000)
+    {
+        Console.WriteLine("Вы ввели не 3-хзначное число");
+        return false;
+    }
+
+    return true;
+}
+
+int digit = ReadInt ("Введите 3-хзначное число => ");
+if (Validate3sign(digit))
+{
+    int secondDigit = (digit / 10) % 10;
+    System.Console.WriteLine($"Вторая цифра числа {digit} равна {secondDigit} ");
+}
