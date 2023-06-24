0ab0089544866054ba9f47c39769461438c4a66d
Vlada <vlada@tar.ru>
2023-06-24 03:25:38 +0300

    Задача 5

---
+++ Lesson1/Task5/Program.cs
@@ -0,0 +1,10 @@
+﻿// Сделайте вычисление модулей числа
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = num;
+if (num < 0) {
+   res = -num;
+}
+Console.WriteLine($"Модуль числа равен {res}");
