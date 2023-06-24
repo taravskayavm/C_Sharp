df30a5463311e8e2e7a7b4d81b187edc189ec7e9
Vlada <vlada@tar.ru>
2023-06-24 04:16:08 +0300

    Домашняя задача 3

---
+++ HW/Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, является ли оно четным (делится ли на два без остатка)
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Clear();
+Console.WriteLine("Введите число => ");
+string number = Console.ReadLine();
+int num = Convert.ToInt32(number);
+
+if (num % 2 == 0) {
+Console.WriteLine("Число четное");
+} else {
+Console.WriteLine("Число нечетное");
+}
