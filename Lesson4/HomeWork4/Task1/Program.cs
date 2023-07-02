ff9525a10e7b74af01bf4594389c8c67062b5f94
Vlada <vlada@tar.ru>
2023-07-02 12:59:14 +0300

    Домашняя работа 4, задача 2

---
+++ Lesson4/HomeWork4/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (a > 0)
+{
+int num = a % 10;
+a = a / 10;
+sum = sum + num;
+}
+Console.WriteLine("сумма всех цифр в числе равна: " + sum);
