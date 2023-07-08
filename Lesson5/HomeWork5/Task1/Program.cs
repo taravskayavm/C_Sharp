5f335b5b5f9f81f245e8aa7ee41629f6c1497d92
Vlada <vlada@tar.ru>
2023-07-08 13:23:01 +0300

    Домашняя работа 5, задача 1

---
+++ Lesson5/HomeWork5/Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+Console.Clear();
+
+Console.WriteLine("Задайте размер массива: ");
+int arrSize = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[arrSize];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Массив: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int a = 0; a < numbers.Length; a++)
+    if (numbers[a] % 2 == 0)
+        count++;
+
+Console.WriteLine($"Всего {numbers.Length} чисел, {count} из них чётные");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100, 1000);
+    }
+}
+
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
