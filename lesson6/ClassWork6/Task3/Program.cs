e64826799ae6752a22c492699353c63efd36c7ac
Vlada <vlada@tar.ru>
2023-07-09 11:27:19 +0300

    Урок 6, задача 3 (массив)

---
+++ lesson6/ClassWork6/Task3/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 3: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+Console.Clear();
+
+int ReadInt(string message)
+{
+    System.Console.WriteLine($"{message}: ");
+    string inputedString = Console.ReadLine();
+    if (int.TryParse(inputedString, out int convertedInt))
+    {
+        return convertedInt;
+    }
+
+    System.Console.WriteLine("Вы ввели не число");
+    Environment.Exit(0);
+    return 0;
+}
+
+int[] GenFib(int num)
+{
+    int[] array = new int[num];
+    array[0] = 0;
+    if (num == 1) return array;
+    array[1] = 1;
+    for (int i = 2; i < array.Length; i++)
+    {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+    return array;
+}
+
+
+
+void ShowArray(int[] arr)
+{
+    foreach (int item in arr)
+    {
+        System.Console.Write($"{item} ");
+    }
+}
+
+int number = ReadInt("Введите число ");
+ShowArray(GenFib(number));
