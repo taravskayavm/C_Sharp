e191c7bdcb052ffbaea1c114bd8534a9af9fc27d
Vlada <vlada@tar.ru>
2023-07-12 22:25:19 +0300

    Урок 7, задача 1

--- Lesson7/ClassWork/Task1/Program.cs
+++ Lesson7/ClassWork/Task1/Program.cs
@@ -1,1 +1,52 @@
-﻿
+﻿// Задача 1: Задайте двумерный массив размера m на n, 
+//каждый элемент в массиве находится по формуле: Aij = i+j. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+Console.Clear();
+
+int[,] CreateArr(int rows, int columns)
+{
+    int[,] arrayD2 = new int[rows, columns];
+
+    for (int i = 0; i < arrayD2.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayD2.GetLength(1); j++)
+        {
+            arrayD2[i, j] = i + j;
+        }
+    }
+    return arrayD2;
+}
+
+void ShowArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int ReadInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    string inputedString = Console.ReadLine();
+    if (int.TryParse(inputedString, out int convertedInt))
+    {
+        return convertedInt;
+    }
+
+    System.Console.WriteLine("Вы ввели не число");
+    Environment.Exit(0);
+    return 0;
+}
+
+int rows = ReadInt("Введите число строк ");
+int columns = ReadInt("Введите число столбцов ");
+ShowArray(CreateArr(rows, columns));
