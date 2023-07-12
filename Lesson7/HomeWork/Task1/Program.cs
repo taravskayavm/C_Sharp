c9cc00063acf77113afd6805e1c36ac08bf3ba64
Vlada <vlada@tar.ru>
2023-07-13 00:35:03 +0300

    Домашняя работа 7, задача 1

---
+++ Lesson7/HomeWork/Task1/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Clear();
+
+double[,] CreateArr(int rows, int columns)
+{
+    double[,] array = new double[rows, columns];
+
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.NextDouble() * 100;
+            Console.Write("{0,6:F2}", array[i, j]);
+        }
+    }
+    return array;
+}
+
+void ShowArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int ReadInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    string inputedString = Console.ReadLine();
+    if (int.TryParse(inputedString, out int convertedInt))
+    {
+        return convertedInt;
+    }
+
+    System.Console.WriteLine("Вы ввели не число");
+    Environment.Exit(0);
+    return 0;
+}
+
+int rows = ReadInt("Введите число строк ");
+int columns = ReadInt("Введите число столбцов ");
+ShowArray(CreateArr(rows, columns));
