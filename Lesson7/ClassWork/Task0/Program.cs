4f3f2298176ab9f8fee6769bf9283903cc1f8e54
Vlada <vlada@tar.ru>
2023-07-12 21:01:50 +0300

    Урок 7, задача 1

---
+++ Lesson7/ClassWork/Task0/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1     4    8   19
+// 5   -2  33   -2
+// 77  3    8     1
+
+Console.Clear();
+
+int[,] CreateArr(int rows, int columns)
+{
+    int[,] arrayD2 = new int[rows, columns];
+
+    for (int i = 0; i < arrayD2.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayD2.GetLength(1); j++)
+        {
+            arrayD2[i, j] = new Random().Next(0, 255);
+        }
+    }
+    return arrayD2;
+}
+
+void ShowArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int ReadInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    string inputedString = Console.ReadLine();
+    if (int.TryParse(inputedString, out int convertedInt))
+    {
+        return convertedInt;
+    }
+
+    System.Console.WriteLine("Вы ввели не число");
+    Environment.Exit(0);
+    return 0;
+}
+
+int rows = ReadInt("Введите число строк ");
+int columns = ReadInt("Введите число столбцов ");
+ShowArray(CreateArr(rows, columns));
