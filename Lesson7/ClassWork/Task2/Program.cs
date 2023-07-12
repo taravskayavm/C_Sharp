693368f964138f8858227429b608c6dd3b24adea
Vlada <vlada@tar.ru>
2023-07-12 22:47:02 +0300

    Урок 7, задача 2

---
+++ Lesson7/ClassWork/Task2/Program.cs
@@ -0,0 +1,74 @@
+﻿//Задача 2: Задайте двумерный массив. Найдите элементы, у которых обе позиции чётные, и замените эти элементы на их квадраты.
+
+// Например, изначально массив
+//  выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// Новый массив будет выглядеть 
+// вот так:
+// 1   4  7  2
+// 5  81  2  9
+// 8   4  2  4
+
+Console.Clear();
+
+int[,] CreateArr(int rows, int columns)
+{
+    int[,] arrayD2 = new int[rows, columns];
+
+    for (int i = 0; i < arrayD2.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayD2.GetLength(1); j++)
+        {
+            arrayD2[i, j] = i + j;
+        }
+    }
+    return arrayD2;
+}
+
+void ShowArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int ReadInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    string inputedString = Console.ReadLine();
+    if (int.TryParse(inputedString, out int convertedInt))
+    {
+        return convertedInt;
+    }
+
+    System.Console.WriteLine("Вы ввели не число");
+    Environment.Exit(0);
+    return 0;
+}
+
+int[,] ModifyArr(int[,] arr)
+{
+    for (int i = 1; i < arr.GetLength(0); i += 2)
+    {
+        for (int j = 1; j < arr.GetLength(1); j += 2)
+        {
+            arr[i, j] *= arr[i, j];
+        }
+    }
+    return arr;
+}
+
+int rows = ReadInt("Введите число строк ");
+int columns = ReadInt("Введите число столбцов ");
+int[,] array = CreateArr(rows, columns);
+ShowArray(array);
+System.Console.WriteLine();
+ShowArray(ModifyArr(array));
