b5de968ff5a43083364bda57fdc6a4b83ddf40b6
Vlada <vlada@tar.ru>
2023-07-12 23:06:08 +0300

    Урок 7, задача 3

---
+++ Lesson7/ClassWork/Task3/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 3: Задайте двумерный массив. Найдите сумму элементов главной диагонали.
+// (Число строк должно быть равно числу столбцов).
+// Например, задан массив:
+// 1   4   7
+// 5   9   2
+// 8   4   2
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+Console.Clear();
+
+int[,] CreateArr(int size)
+{
+    int[,] arrayD2 = new int[size, size];
+
+    for (int i = 0; i < arrayD2.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayD2.GetLength(1); j++)
+        {
+            arrayD2[i, j] = new Random().Next(0, 5);
+        }
+    }
+    return arrayD2;
+}
+
+void ShowArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int ReadInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    string inputedString = Console.ReadLine();
+    if (int.TryParse(inputedString, out int convertedInt))
+    {
+        return convertedInt;
+    }
+
+    System.Console.WriteLine("Вы ввели не число");
+    Environment.Exit(0);
+    return 0;
+}
+
+int SumMainDiagArr(int[,] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += array[i, i];
+    }
+    return sum;
+}
+
+int size = ReadInt("Введите размер массива ");
+int[,] array = CreateArr(size);
+ShowArray(array);
+System.Console.WriteLine($"Сумма элементов главной диагонали: {SumMainDiagArr(array)}");
+
