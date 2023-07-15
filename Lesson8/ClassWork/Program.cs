d3c0deda04159d52a69df801181bf8c2ffb23958
Vlada <vlada@tar.ru>
2023-07-15 20:12:28 +0300

    Урок 8, задача 1

---
+++ Lesson8/ClassWork/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 1: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+Console.Clear();
+
+int[,] CreateArr(int rows, int columns)
+{
+    int[,] arrayD2 = new int[rows, columns];
+
+    for (int i = 0; i < arrayD2.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayD2.GetLength(1); j++)
+        {
+            arrayD2[i, j] = new Random().Next(0, 255);
+        }
+    }
+    return arrayD2;
+}
+
+void ShowArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int ReadInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    string inputedString = Console.ReadLine();
+    if (int.TryParse(inputedString, out int convertedInt))
+    {
+        return convertedInt;
+    }
+
+    System.Console.WriteLine("Вы ввели не число");
+    Environment.Exit(0);
+    return 0;
+}
+
+int[,] ChangeRowsArray(int[,] arr)
+{
+    int temp;
+    int lastRow = arr.GetLength(0) - 1;
+    for (int i = 0; i < arr.GetLength(1); i++)
+    {
+        temp = arr[0, i];
+        arr[0, i] = arr[lastRow, i];
+        arr[lastRow, i] = temp;
+    }
+    return arr;
+}
+
+int rows = ReadInt("Введите число строк ");
+int columns = ReadInt("Введите число столбцов ");
+int[,] array = CreateArr(rows, columns);
+ShowArray(array);
+System.Console.WriteLine();
+ShowArray(ChangeRowsArray(array));
