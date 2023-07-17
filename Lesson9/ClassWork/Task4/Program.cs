61e5c02763384a73403efcf19931fcbde02360fe
Vlada <vlada@tar.ru>
2023-07-17 14:46:15 +0300

    Урок 9, задача 4

--- Lesson9/ClassWork/Task4/Program.cs
+++ Lesson9/ClassWork/Task4/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.Clear();
+
+int ReadInput(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int A = ReadInput("Enter A:");
+int B = ReadInput("Enter B:");
+
+int Fn(int A, int B)
+{
+    if (B == 0) return 1;
+    return A * Fn(A, B - 1);
+}
+
+System.Console.WriteLine(Fn(A, B));
