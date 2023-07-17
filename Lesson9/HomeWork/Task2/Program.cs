848ebea17a9a86d81b749d4102064661b374fa7c
Vlada <vlada@tar.ru>
2023-07-17 15:41:33 +0300

    Домашняя работа 9, задача 2

---
+++ Lesson9/HomeWork/Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+Console.Clear();
+
+int ReadInput(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int M = ReadInput("Введите натуральное число: ");
+int N = ReadInput("Введите натуральное число: ");
+
+void PrintNaturalNumes(int M, int N)
+{
+    Console.Write(SumMN(M - 1, N));
+}
+
+int SumMN(int M, int N)
+{
+    int res = M;
+    if (M == N)
+        return 0;
+    else
+    {
+        M++;
+        res = M + SumMN(M, N);
+        return res;
+    }
+}
+PrintNaturalNumes(M, N);
