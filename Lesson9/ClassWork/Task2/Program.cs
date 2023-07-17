00f0e942d9c7593f12702f465d83f7e8f7fb969a
Vlada <vlada@tar.ru>
2023-07-17 13:41:37 +0300

    Урок 9, задача 2

---
+++ Lesson9/ClassWork/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая рекурсивно выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+Console.Clear();
+
+int ReadInput(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int M = ReadInput("Введите натуральное число: ");
+int N = ReadInput("Введите натуральное число: ");
+
+void PrintNaturalNumes(int M, int N)
+{
+    if (M > N) return;
+    System.Console.WriteLine(M);
+    PrintNaturalNumes(M + 1, N);
+}
+
+PrintNaturalNumes(M, N);
