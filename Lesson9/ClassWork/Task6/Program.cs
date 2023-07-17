675d1bc1fb0f67fc02383a09b0e28845de3cdfb6
Vlada <vlada@tar.ru>
2023-07-17 15:16:44 +0300

    Урок 9, задача 6

--- Lesson9/ClassWork/Task6/Program.cs
+++ Lesson9/ClassWork/Task6/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Проверка на простое число: 
+// N = 13 -> "Это простое число"
+// N = 12 -> “Это не простое число”
+
+Console.Clear();
+
+int ReadInput(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int A = ReadInput("Enter A: ");
+
+bool Fn(int A, int del = 0)
+{
+    if (del == 0) del = A - 1;
+    if (del == 1) return true;
+    return A % del != 0 && Fn(A, del - 1);
+}
+
+System.Console.WriteLine(Fn(A));
