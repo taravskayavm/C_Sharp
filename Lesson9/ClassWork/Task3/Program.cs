7f52b2e2877dadd14cacf0677fa214945f2bedb8
Vlada <vlada@tar.ru>
2023-07-17 14:39:10 +0300

    Урок 9, задача 3

--- Lesson9/ClassWork/Task3/Program.cs
+++ Lesson9/ClassWork/Task3/Program.cs
@@ -2,3 +2,21 @@
 // 453 -> 12
 // 45 -> 9
 
+Console.Clear();
+
+int ReadInput(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int M = ReadInput("Введите натуральное число: ");
+
+int PrintNaturalNumes(int M)
+{
+    if (M == 0) return 0;
+    return M % 10 + PrintNaturalNumes(M / 10);
+}
+
+System.Console.WriteLine(PrintNaturalNumes(M));
+
