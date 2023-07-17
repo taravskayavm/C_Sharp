887bf6e168140663878942d58d6076fa67fd11df
Vlada <vlada@tar.ru>
2023-07-17 14:57:20 +0300

    Урок 9, задача 5

--- Lesson9/ClassWork/Task5/Program.cs
+++ Lesson9/ClassWork/Task5/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 5: Определите, является ли число степенью двойки:
+// N = 16 -> "Является степень двойки"
+// N = 12 -> “Не является степенью двойки”
+
+Console.Clear();
+
+int ReadInput(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int A = ReadInput("Enter A:");
+
+bool Fn(int A)
+{
+    if (A == 2) return true;
+    return A % 2 == 0 && Fn(A / 2);
+}
+
+System.Console.WriteLine(Fn(A));
